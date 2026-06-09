aa00f089dc2085c5835bbda2ab80a6d2563e3ced
sleepy-y <p.chumski@gmail.com>
2026-06-09 02:05:56 +0200

    up

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import argparse
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
+import random
+
+
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 # Importy autorskich modułów
 from utils.dataset import GeometricDataset, BifurcationDataset12, HardcoreUSGDataset
